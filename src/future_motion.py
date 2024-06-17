2464297aa228f59ccd76d61093ac35d6e4075974
roydenwa <royden-wagner@outlook.com>
2024-06-18 00:28:29 +0200

    Add visualization flags

--- src/future_motion.py
+++ src/future_motion.py
@@ -12,6 +12,7 @@ from typing import Dict, List, Tuple, Optional
 from models.modules.neural_collapse import OnlineLinearClassifier
 from models.metrics.representation_eval import std_of_l2_normalized
 from data.lang_labels import agent_dict, direction_dict, speed_dict, acceleration_dict
+from data.plot_3d import plot_motion_forecasts, mplfig_to_npimage, tensor_dict_to_cpu
 
 
 class FutureMotion(LightningModule):
@@ -35,6 +36,8 @@ class FutureMotion(LightningModule):
         interactive_challenge: bool = False,
         wb_artifact: Optional[str] = None,
         measure_neural_collapse: bool = False,
+        plot_motion: bool = True,
+        control_temperatures: list = [-20, -10, 0, 10, 20],
     ) -> None:
         super().__init__()
         self.save_hyperparameters()
@@ -88,7 +91,6 @@ class FutureMotion(LightningModule):
                 self.online_classifiers_speed.append(OnlineLinearClassifier(feature_dim=self.model.hidden_dim, num_classes=len(speed_dict), topk=(1, 2), log_prefix=f"speed_{idx_hidden}"))
                 self.online_classifiers_acceleration.append(OnlineLinearClassifier(feature_dim=self.model.hidden_dim, num_classes=len(acceleration_dict), topk=(1, 2), log_prefix=f"acceleration_{idx_hidden}"))
                 self.online_classifiers_motion.append(OnlineLinearClassifier(feature_dim=self.model.hidden_dim, num_classes=n_all_classes, log_prefix=f"motion_{idx_hidden}"))
-
 
     def training_step(self, batch: Dict[str, Tensor], batch_idx: int) -> Dict:
         with torch.no_grad():
@@ -179,6 +181,7 @@ class FutureMotion(LightningModule):
                 inference_cache_map=self.hparams.inference_cache_map,
                 **input_dict,
             )
+            
             for idx_hidden in range(3):
                 cls_dir_loss, cls_dir_log = self.online_classifiers_direction[idx_hidden].validation_step(
                     (target_embs[idx_hidden][batch["input/agent_mask"]].mean(dim=1), batch["input/direction_labels"].to("cuda")[batch["input/agent_mask"]]), batch_idx
@@ -220,6 +223,33 @@ class FutureMotion(LightningModule):
         # ! post-processing
         # for _ in range(self.hparams.inference_repeat_n):
         pred_dict = self.post_processing(pred_dict)
+
+        if self.hparams.plot_motion and batch_idx < 3:
+            wandb_imgs = []
+            fig = plot_motion_forecasts(tensor_dict_to_cpu(batch), pred_dict=tensor_dict_to_cpu(pred_dict))
+            np_img = mplfig_to_npimage(fig)
+            wandb_imgs.append(wandb.Image(np_img, caption=f"forecasts"))
+
+            # if self.hparams.control_past_motion:
+            if self.hparams.control_temperatures is not None:
+                # for tau in list(range(-100, 105, 50)):
+                for tau in self.hparams.control_temperatures:
+                    self.model.intra_class_encoder.control_temperature = tau
+
+                    pred_dict["pred_valid"], pred_dict["pred_conf"], pred_dict["pred"], target_embs = self.model(
+                        inference_repeat_n=self.hparams.inference_repeat_n,
+                        inference_cache_map=self.hparams.inference_cache_map,
+                        **input_dict,
+                    )
+                    pred_dict = self.post_processing(pred_dict)
+                    fig = plot_motion_forecasts(tensor_dict_to_cpu(batch), pred_dict=tensor_dict_to_cpu(pred_dict), 
+                                                save_path=f"/home/wagner/tmp_data/words_in_motion/debug_plots/forecasts_{batch_idx}_{tau}.png")
+                    np_img = mplfig_to_npimage(fig)
+                    wandb_imgs.append(wandb.Image(np_img, caption=f"forecasts w/ tau = {tau}"))
+                
+                self.model.intra_class_encoder.control_temperature = 0
+            
+            self.logger[0].experiment.log({f"motion forecasts batch {batch_idx}": wandb_imgs}, commit=False)
 
         if self.hparams.inference_repeat_n > 1:
             return  # measuring FPS for online inference.
