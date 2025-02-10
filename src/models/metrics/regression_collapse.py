72fe7615a7eb7bc1c334c2b411018025ad66d961
Felix Hauser <felix.hauser@kit.edu>
2025-02-10 13:02:21 +0100

    Add NRC1 metric

---
+++ src/models/metrics/regression_collapse.py
@@ -0,0 +1,8 @@
+import nc_toolbox as nctb
+from torch import Tensor
+
+
+def nrc1_feature_collapse(hidden_states_0: Tensor, dim_output: int) -> float:
+    H = hidden_states_0.cpu().numpy()  # n_sample x d_feature
+    assert dim_output <= hidden_states_0.shape[1]
+    return nctb.nrc1_collapse(H, dim_output)
